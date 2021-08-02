904267a220194758f27dd060766adfc6d47e9b57
Mark <mark3fullen@gmail.com>
2021-08-02 10:26:39 -0400

    Added Header, Aside, some links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>Bruh Moment</h1>
+      <header className="Header">
+          <h1>Welcome to the Borough!</h1>
+      </header>
+      <aside className="Aside">
+        <ul>
+          <li>
+            Link 1
+          </li>
+          <li>
+            Link 2
+          </li>
+          <li>
+            Link 3
+          </li>
+        </ul>
+      </aside>
+      <BrowserRouter>
+        <Route path="/">
+          <Home />
+        </Route>
+     </BrowserRouter>
     </div>
+    
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>This is the home page</h1>
+            <p>This is the home page info</p>
+        </>
+    )
+}
+
+export default Home
